b60e2338297a4faa57d0921fa57766e2f70c43c1
Christof Weickhardt <chweicki@icloud.com>
2021-05-03 01:22:47 +0200

    fix: add type tranformation

--- src/app-search.query.ts
+++ src/app-search.query.ts
@@ -1,11 +1,14 @@
+import { Type } from 'class-transformer';
 import { IsBoolean, IsInt, IsPositive, IsString } from 'class-validator';
 
 export class AppSearchQuery {
   @IsString()
   public name: string;
   @IsBoolean()
+  @Type(() => Boolean)
   public external: boolean;
   @IsInt()
   @IsPositive()
+  @Type(() => Number)
   public age: number;
 }
--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,15 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import { Body, Controller, Get, Post, Query } from '@nestjs/common';
 import { AppSearchQuery } from './app-search.query';
 
 @Controller()
 export class AppController {
   @Get()
-  getHello(@Query() query: AppSearchQuery): AppSearchQuery {
+  querySearch(@Query() query: AppSearchQuery): AppSearchQuery {
+    return query;
+  }
+
+  @Post('')
+  bodySearch(@Body() query: AppSearchQuery): AppSearchQuery {
     return query;
   }
 }
